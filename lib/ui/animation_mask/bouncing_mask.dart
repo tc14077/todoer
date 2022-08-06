e56ebbc434f971e92a51c0349cd3c53f3465ad57
tc14077 <tc14077222@gmail.com>
2022-08-06 10:18:47 +0800

    feat: add optional field to mask

--- lib/ui/animation_mask/bouncing_mask.dart
+++ lib/ui/animation_mask/bouncing_mask.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 class BouncingMask extends StatefulWidget {
   final Widget child;
+  final Duration animationDuration;
 
-  const BouncingMask({super.key, required this.child});
+  const BouncingMask({
+    super.key,
+    required this.child,
+    this.animationDuration = const Duration(seconds: 2),
+  });
 
   @override
   State<StatefulWidget> createState() {
@@ -14,11 +19,11 @@ class BouncingMask extends StatefulWidget {
 class _BouncingMaskState extends State<BouncingMask>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 2),
+    duration: widget.animationDuration,
     vsync: this,
   )..forward();
   late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-    begin: Offset(0, -1),
+    begin: const Offset(0, -1),
     end: const Offset(0, 0),
   ).animate(CurvedAnimation(
     parent: _controller,

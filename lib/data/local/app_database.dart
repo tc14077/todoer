17efdc4dd90f29b4c884bcbd35a1889efd278506
Alex Lam <alexlam017@gmail.com>
2022-08-07 12:45:22 +0800

    feat: setup app database

---
+++ lib/data/local/app_database.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [])
+class MyDatabase extends _$MyDatabase {
+  // we tell the database where to store the data with this constructor
+  MyDatabase() : super(_openConnection());
+
+  // you should bump this number whenever you change or add a table definition.
+  // Migrations are covered later in the documentation.
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'app_database.sqlite'));
+    return NativeDatabase(file);
+  });
+}

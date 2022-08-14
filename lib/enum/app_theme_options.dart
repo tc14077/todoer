bda1aeff16adb29924de3bf76013b0eaddcdf610
tc14077 <tc14077222@gmail.com>
2022-08-14 13:55:20 +0800

    feat: comment out the unsupported theme options

--- lib/enum/app_theme_options.dart
+++ lib/enum/app_theme_options.dart
@@ -3,9 +3,9 @@ import 'package:todoer/ui/system/themes.dart';
 
 enum AppThemeOption {
   light(themeMode: ThemeMode.light, displayName: 'Day theme'),
-  dark(themeMode: ThemeMode.dark, displayName: 'Dark theme'),
-  system(themeMode: ThemeMode.system, displayName: 'System theme'),
-  custom(themeMode: null, displayName: 'Custom theme');
+  dark(themeMode: ThemeMode.dark, displayName: 'Dark theme');
+  // system(themeMode: ThemeMode.system, displayName: 'System theme'),
+  // custom(themeMode: null, displayName: 'Custom theme');
 
   final ThemeMode? themeMode;
   final String displayName;
@@ -17,9 +17,9 @@ enum AppThemeOption {
         return AppTheme.lightTheme;
       case AppThemeOption.dark:
         return AppTheme.darkTheme;
-      case AppThemeOption.system:
-      case AppThemeOption.custom:
-        return null;
+      // case AppThemeOption.system:
+      // case AppThemeOption.custom:
+      //   return null;
     }
   }
 }

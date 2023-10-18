66d5cb0e761a367e1b7bceb1a2aa58c0beaaf571
tc14077 <tc14077222@gmail.com>
2023-10-18 22:30:38 +0800

    wip

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todoer/ui/system/themed_text.dart';
 
+import '../widget/table_calendar_widget.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -14,11 +16,40 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Plannable Booking'),
       ),
-      body: Center(
-        child: HeadlineMediumText(
-          widget.title,
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black,
+                padding: const EdgeInsets.all(16.0),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {
+                final widgetWidth = context.size?.width;
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Basic dialog title'),
+                      content: SizedBox(
+                        width: widgetWidth!,
+                        child: TableCalendarWidget(
+                          focusedDay: DateTime.now(),
+                          onDaySelected: (day) {},
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: const Text('Gradient'),
+            ),
+          ],
         ),
       ),
     );

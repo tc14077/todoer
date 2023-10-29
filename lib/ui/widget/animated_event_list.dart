f6abc0d1414cefc44dd4435e4850840b335293ac
tc14077 <tc14077222@gmail.com>
2023-10-29 12:27:54 +0800

    feat: add AnimatedEventList for displaying event cards

---
+++ lib/ui/widget/animated_event_list.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:todoer/ui/screen/home_screen.dart';
+
+import 'event_card.dart';
+
+class AnimatedEventList extends StatelessWidget {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  final List<Event> events;
+  late final ListModel<Event> _list;
+
+  AnimatedEventList({
+    super.key,
+    required this.events,
+  }) {
+    _list = ListModel<Event>(
+      listKey: _listKey,
+      removedItemBuilder: _buildRemovedItem,
+      initialItems: events,
+    );
+  }
+
+  // Used to build list items that haven't been removed.
+  Widget _buildItem(
+    BuildContext context,
+    int index,
+    Animation<double> animation,
+  ) {
+    return EventCard(
+      animation: animation,
+      event: _list[index],
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedList(
+      key: _listKey,
+      initialItemCount: _list.length,
+      itemBuilder: _buildItem,
+    );
+  }
+
+  /// The builder function used to build items that have been removed.
+  ///
+  /// Used to build an item after it has been removed from the list. This method
+  /// is needed because a removed item remains visible until its animation has
+  /// completed (even though it's gone as far as this ListModel is concerned).
+  /// The widget will be used by the [AnimatedListState.removeItem] method's
+  /// [AnimatedRemovedItemBuilder] parameter.
+  Widget _buildRemovedItem(
+    Event item,
+    BuildContext context,
+    Animation<double> animation,
+  ) {
+    return EventCard(
+      animation: animation,
+      event: item,
+      // No gesture detector here: we don't want removed items to be interactive.
+    );
+  }
+}
+
+/// Keeps a Dart [List] in sync with an [AnimatedList].
+///
+/// The [insert] and [removeAt] methods apply to both the internal list and
+/// the animated list that belongs to [listKey].
+///
+/// This class only exposes as much of the Dart List API as is needed by the
+/// sample app. More list methods are easily added, however methods that
+/// mutate the list must make the same changes to the animated list in terms
+/// of [AnimatedListState.insertItem] and [AnimatedList.removeItem].
+class ListModel<E> {
+  ListModel({
+    required this.listKey,
+    required this.removedItemBuilder,
+    Iterable<E>? initialItems,
+  }) : _items = List<E>.from(initialItems ?? <E>[]);
+
+  final GlobalKey<AnimatedListState> listKey;
+  final RemovedItemBuilder<E> removedItemBuilder;
+  final List<E> _items;
+
+  AnimatedListState? get _animatedList => listKey.currentState;
+
+  void insert(int index, E item) {
+    _items.insert(index, item);
+    _animatedList!.insertItem(index);
+  }
+
+  E removeAt(int index) {
+    final E removedItem = _items.removeAt(index);
+    if (removedItem != null) {
+      _animatedList!.removeItem(
+        index,
+        (BuildContext context, Animation<double> animation) {
+          return removedItemBuilder(removedItem, context, animation);
+        },
+      );
+    }
+    return removedItem;
+  }
+
+  int get length => _items.length;
+
+  E operator [](int index) => _items[index];
+
+  int indexOf(E item) => _items.indexOf(item);
+}
+
+typedef RemovedItemBuilder<T> = Widget Function(
+  T item,
+  BuildContext context,
+  Animation<double> animation,
+);

4ef8236d524cf76f5f7dd1ed09193709dec651c1
tc14077 <tc14077222@gmail.com>
2023-10-29 12:28:28 +0800

    feat: add mock event ui

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:todoer/ui/system/themed_text.dart';
+import 'package:todoer/ui/widget/animated_event_list.dart';
 
 import '../widget/table_calendar_widget.dart';
 
@@ -11,6 +12,69 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
+final mocking = [
+  Event(
+    'Booking 1',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 2',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 3',
+    DateTime(2023, 11, 2, 13, 00),
+  ),
+  Event(
+    'Booking 1',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 2',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 3',
+    DateTime(2023, 11, 2, 13, 00),
+  ),
+  Event(
+    'Booking 1',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 2',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 3',
+    DateTime(2023, 11, 2, 13, 00),
+  ),
+  Event(
+    'Booking 1',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 2',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 3',
+    DateTime(2023, 11, 2, 13, 00),
+  ),
+  Event(
+    'Booking 1',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 2',
+    DateTime(2023, 11, 1, 12, 00),
+  ),
+  Event(
+    'Booking 3',
+    DateTime(2023, 11, 2, 13, 00),
+  ),
+];
 
 class _HomeScreenState extends State<HomeScreen> {
   final _selectedDay = DateTime.now();
@@ -38,8 +102,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   context: context,
                   builder: (context) {
                     return AlertDialog(
-                      title:
-                          Center(child: BodyLargeText(selectedDayTimeString)),
+                      title: Center(
+                        child: BodyLargeText(selectedDayTimeString),
+                      ),
                       content: SizedBox(
                         width: widgetWidth!,
                         child: TableCalendarWidget(
@@ -65,9 +130,23 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               child: BodyLargeText(selectedDayTimeString),
             ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: AnimatedEventList(events: mocking),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+// MOCKING
+class Event {
+  final String name;
+  final DateTime time;
+
+  Event(this.name, this.time);
 }

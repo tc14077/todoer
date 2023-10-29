e55d1315603b4ce40910281b2f0880329539d84f
tc14077 <tc14077222@gmail.com>
2023-10-29 13:04:35 +0800

    feat: add new model invitee

--- lib/data/models/event.dart
+++ lib/data/models/event.dart
@@ -1,7 +1,10 @@
 // MOCKING
+import 'package:todoer/data/models/invitee.dart';
+
 class Event {
   final String name;
   final DateTime time;
+  final List<Invitee>? invitees;
 
-  Event(this.name, this.time);
+  Event(this.name, this.time, {this.invitees});
 }

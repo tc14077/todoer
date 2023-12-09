6d367567f40624ddec5ee990fe150b92ba347201
tc14077 <tc14077222@gmail.com>
2023-12-09 15:46:16 +0800

    feat: add DateTimePicker

--- lib/ui/widget/create_booking_widget.dart
+++ lib/ui/widget/create_booking_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todoer/ui/system/themed_text.dart';
+
+import 'invitee_form.dart';
 
 class CreateBookingWidget extends StatelessWidget {
   CreateBookingWidget({
@@ -21,6 +24,7 @@ class CreateBookingWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const TitleLargeText('Booking Detail'),
         TextField(
           controller: _eventNameController,
           decoration: const InputDecoration(
@@ -33,63 +37,94 @@ class CreateBookingWidget extends StatelessWidget {
             labelText: 'Remark (Optional)',
           ),
         ),
-        Row(
-          children: [
-            Flexible(
-              flex: 1,
-              child: TextField(
-                controller: _eventNameController,
-                decoration: const InputDecoration(
-                  labelText: 'Booking Date',
-                  suffixIcon: Icon(Icons.calendar_month),
+        DateTimePicker(
+          dateController: _eventNameController,
+          defaultBookingTime: defaultBookingTime,
+        ),
+        const SizedBox.square(
+          dimension: 12,
+        ),
+        InviteeForm(
+          inviteeName: 'Tom',
+          inviteePhoneNumber: '91466915',
+          onInviteeInfoUpdate: (name, phoneNumber) {
+            print('$name, $phoneNumber');
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class DateTimePicker extends StatelessWidget {
+  DateTimePicker({
+    super.key,
+    required TextEditingController dateController,
+    required this.defaultBookingTime,
+  }) : _dateController = dateController;
+
+  final TextEditingController _dateController;
+  final DateTime defaultBookingTime;
+  final DateTime today = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          flex: 1,
+          child: TextField(
+            controller: _dateController,
+            decoration: const InputDecoration(
+              labelText: 'Booking Date',
+              suffixIcon: Icon(Icons.calendar_month),
+            ),
+            readOnly: true,
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: defaultBookingTime,
+                firstDate: today.subtract(const Duration(days: 1)),
+                lastDate: today.add(const Duration(days: 365)),
+                currentDate: today,
+                initialEntryMode: DatePickerEntryMode.calendarOnly,
+                initialDatePickerMode: DatePickerMode.day,
+              );
+              print(date);
+            },
+          ),
+        ),
+        const SizedBox.square(
+          dimension: 8.0,
+        ),
+        Flexible(
+          flex: 1,
+          child: TextField(
+            controller: _dateController,
+            decoration: const InputDecoration(
+              labelText: 'Booking Time',
+              suffixIcon: Icon(Icons.alarm),
+            ),
+            readOnly: true,
+            onTap: () async {
+              final selectedTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay(
+                  hour: defaultBookingTime.hour,
+                  minute: defaultBookingTime.minute,
                 ),
-                readOnly: true,
-                onTap: () async {
-                  final date = await showDatePicker(
-                    context: context,
-                    initialDate: defaultBookingTime,
-                    firstDate: DateTime.now().subtract(const Duration(days: 1)),
-                    lastDate: DateTime.now().add(const Duration(days: 365)),
-                    initialEntryMode: DatePickerEntryMode.calendarOnly,
-                    initialDatePickerMode: DatePickerMode.day,
+                initialEntryMode: TimePickerEntryMode.inputOnly,
+                builder: (BuildContext context, Widget? child) {
+                  return MediaQuery(
+                    data: MediaQuery.of(context)
+                        .copyWith(alwaysUse24HourFormat: true),
+                    child: child!,
                   );
-                  print(date);
                 },
-              ),
-            ),
-            const SizedBox.square(
-              dimension: 8.0,
-            ),
-            Flexible(
-              flex: 1,
-              child: TextField(
-                controller: _eventNameController,
-                decoration: const InputDecoration(
-                  labelText: 'Booking Time',
-                  suffixIcon: Icon(Icons.alarm),
-                ),
-                readOnly: true,
-                onTap: () async {
-                  final selectedTime = await showTimePicker(
-                    context: context,
-                    initialTime: TimeOfDay(
-                      hour: defaultBookingTime.hour,
-                      minute: defaultBookingTime.minute,
-                    ),
-                    initialEntryMode: TimePickerEntryMode.inputOnly,
-                    builder: (BuildContext context, Widget? child) {
-                      return MediaQuery(
-                        data: MediaQuery.of(context)
-                            .copyWith(alwaysUse24HourFormat: true),
-                        child: child!,
-                      );
-                    },
-                  );
-                  print(selectedTime);
-                },
-              ),
-            ),
-          ],
+              );
+              print(selectedTime);
+            },
+          ),
         ),
       ],
     );

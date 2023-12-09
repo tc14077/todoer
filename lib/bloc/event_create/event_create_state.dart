4bace682fdbd8bd56c5d25d09f68099bce8c8df9
tc14077 <tc14077222@gmail.com>
2023-12-09 16:35:37 +0800

    feat: add EventCreateBloc

---
+++ lib/bloc/event_create/event_create_state.dart
@@ -0,0 +1,35 @@
+part of 'event_create_bloc.dart';
+
+sealed class EventCreateState extends Equatable {
+  const EventCreateState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class EventCreateInitializeInProgress extends EventCreateState {}
+
+final class EventDataUpdateSuccess extends EventCreateState {
+  final String? name;
+  final String? remark;
+  final DateTime? selectedDate;
+  final TimeOfDay? selectedTime;
+  final List<InviteePair> inviteePairList;
+
+  const EventDataUpdateSuccess({
+    required this.name,
+    required this.remark,
+    required this.selectedDate,
+    required this.selectedTime,
+    required this.inviteePairList,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        remark,
+        selectedDate,
+        selectedTime,
+        inviteePairList,
+      ];
+}

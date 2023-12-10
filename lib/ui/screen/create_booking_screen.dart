42519e58f7a4d306c04f743e32844427da789e94
tc14077 <tc14077222@gmail.com>
2023-12-10 15:10:06 +0800

    feat: link bloc with ui

--- lib/ui/screen/create_booking_screen.dart
+++ lib/ui/screen/create_booking_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoer/bloc/event_create/event_create_bloc.dart';
 import 'package:todoer/ui/widget/create_booking_widget.dart';
 
 import '../system/themed_text.dart';
@@ -8,14 +10,36 @@ class CreateBookingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Booking'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: CreateBookingWidget(
-          defaultBookingTime: DateTime.now(),
+    return BlocProvider<EventCreateBloc>(
+      create: (context) => EventCreateBloc(),
+      child: Builder(
+        builder: (providerContext) => Scaffold(
+          appBar: AppBar(
+            title: const TitleLargeText('Create Booking'),
+            actions: [
+              TextButton(
+                onPressed: () {},
+                child: const BodyMediumText('Save'),
+              )
+            ],
+          ),
+          body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: BlocBuilder<EventCreateBloc, EventCreateState>(
+              builder: (context, state) {
+                if (state is EventDataUpdateSuccess) {
+                  return BookingWidget(
+                    defaultBookingTime: DateTime.now(),
+                    name: state.name,
+                    remark: state.remark,
+                    selectedDate: state.selectedDate,
+                    selectedTime: state.selectedTime,
+                  );
+                }
+                return Container();
+              },
+            ),
+          ),
         ),
       ),
     );

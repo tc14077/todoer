5caed3478b0e89e44cc4e02f7faceae9e851def6
tc14077 <tc14077222@gmail.com>
2023-12-10 15:58:58 +0800

    fix: fix state issue

--- lib/ui/widget/booking_detail/booking_detail_widget.dart
+++ lib/ui/widget/booking_detail/booking_detail_widget.dart
@@ -8,22 +8,43 @@ import 'invitee_form.dart';
 
 part 'date_time_picker_field.dart';
 
-class BookingDetailWidget extends StatelessWidget {
-  BookingDetailWidget({
+class BookingDetailWidget extends StatefulWidget {
+  const BookingDetailWidget({
     super.key,
     required this.defaultBookingTime,
     required this.selectedDate,
     required this.selectedTime,
-    String? name,
-    String? remark,
-  })  : _eventNameController = TextEditingController(text: name),
-        _eventRemarkController = TextEditingController(text: remark);
+    this.bookingName,
+    this.remark,
+  });
+  final String? bookingName;
+  final String? remark;
 
-  final TextEditingController _eventNameController;
-  final TextEditingController _eventRemarkController;
   final DateTime defaultBookingTime;
   final DateTime selectedDate;
   final TimeOfDay selectedTime;
+
+  @override
+  State<BookingDetailWidget> createState() => _BookingDetailWidgetState();
+}
+
+class _BookingDetailWidgetState extends State<BookingDetailWidget> {
+  late final TextEditingController _eventNameController;
+  late final TextEditingController _eventRemarkController;
+  final nameFieldKey = UniqueKey();
+  final remarkFieldKey = UniqueKey();
+
+  @override
+  void initState() {
+    _eventNameController = TextEditingController(text: widget.bookingName);
+    _eventRemarkController = TextEditingController(text: widget.remark);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant BookingDetailWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,28 +53,38 @@ class BookingDetailWidget extends StatelessWidget {
       children: [
         const TitleLargeText('Booking Detail'),
         TextField(
+          key: nameFieldKey,
           controller: _eventNameController,
           decoration: const InputDecoration(
             labelText: 'Booking Name',
           ),
-          onChanged: (value) => context.read<EventCreateBloc>().add(EventDataUpdateRequested(name: value)),
+          onChanged: (value) => context
+              .read<EventCreateBloc>()
+              .add(EventDataUpdateRequested(name: value)),
         ),
         TextField(
+          key: remarkFieldKey,
           controller: _eventRemarkController,
           decoration: const InputDecoration(
             labelText: 'Remark (Optional)',
           ),
-          onChanged: (value) => context.read<EventCreateBloc>().add(EventDataUpdateRequested(remark: value)),
+          onChanged: (value) => context
+              .read<EventCreateBloc>()
+              .add(EventDataUpdateRequested(remark: value)),
         ),
         DateTimePickerField(
           onDateUpdate: (date) {
-            context.read<EventCreateBloc>().add(EventDataUpdateRequested(selectedDate: date));
+            context
+                .read<EventCreateBloc>()
+                .add(EventDataUpdateRequested(selectedDate: date));
           },
           onTimeUpdate: (time) {
-             context.read<EventCreateBloc>().add(EventDataUpdateRequested(selectedTime: time));
+            context
+                .read<EventCreateBloc>()
+                .add(EventDataUpdateRequested(selectedTime: time));
           },
-          pickedDate: selectedDate,
-          pickedTime: selectedTime,
+          pickedDate: widget.selectedDate,
+          pickedTime: widget.selectedTime,
         ),
         const SizedBox.square(
           dimension: 12,

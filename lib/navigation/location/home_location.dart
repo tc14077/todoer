97559e74f1901475b417f0d2d2540f3017b142df
tc14077 <tc14077222@gmail.com>
2023-12-11 14:22:03 +0800

    feat: add new app paths

--- lib/navigation/location/home_location.dart
+++ lib/navigation/location/home_location.dart
@@ -1,13 +1,14 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:todoer/navigation/path/app_path.dart';
+import 'package:todoer/ui/screen/edit_booking_screen.dart';
 
 import '../../ui/screen/create_booking_screen.dart';
 import '../../ui/screen/home_screen.dart';
 
 class HomeLocation extends BeamLocation<BeamState> {
   @override
-  List<Pattern> get pathPatterns => ['${AppPath.home}/*'];
+  List<Pattern> get pathPatterns => [AppPath.editBooking, '${AppPath.home}/*'];
 
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
@@ -20,8 +21,20 @@ class HomeLocation extends BeamLocation<BeamState> {
     if (state.pathPatternSegments.contains('createBooking')) {
       pages.add(
         BeamPage(
-          key: ValueKey('createBooking'),
+          key: const ValueKey('createBooking'),
           child: CreateBookingScreen(),
+        ),
+      );
+    }
+    if (state.pathPatternSegments.contains('editBooking')) {
+      pages.add(
+        BeamPage(
+          key: const ValueKey('editBooking'),
+          child: EditBookingScreen(
+            eventId: int.parse(
+              state.pathParameters['eventId']!,
+            ),
+          ),
         ),
       );
     }

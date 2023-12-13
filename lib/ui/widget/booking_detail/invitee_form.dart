f6a02ddd3178de1130dca54fa239bb8c4d303a2d
tc14077 <tc14077222@gmail.com>
2023-12-13 23:15:58 +0800

    feat: add inviteeCountryCode to InviteeForm

--- lib/ui/widget/booking_detail/invitee_form.dart
+++ lib/ui/widget/booking_detail/invitee_form.dart
@@ -7,16 +7,22 @@ class InviteeForm extends StatefulWidget {
   const InviteeForm({
     super.key,
     this.inviteeName,
+    this.inviteeCountryCode,
     this.inviteePhoneNumber,
     this.eventFormError,
     required this.onInviteeInfoUpdate,
   });
 
   final String? inviteeName;
+  final String? inviteeCountryCode;
   final String? inviteePhoneNumber;
   final Set<EventFormError>? eventFormError;
 
-  final Function(String inviteeName, String phoneNumber) onInviteeInfoUpdate;
+  final Function(
+    String inviteeName,
+    String countryCode,
+    String phoneNumber,
+  ) onInviteeInfoUpdate;
 
   @override
   State<InviteeForm> createState() => _InviteeFormState();
@@ -25,15 +31,21 @@ class InviteeForm extends StatefulWidget {
 class _InviteeFormState extends State<InviteeForm> {
   late final TextEditingController _inviteeNameController;
   late final TextEditingController _inviteePhoneNumberController;
+  late final TextEditingController _inviteeCountryCodeController;
 
   final _nameFieldKey = UniqueKey();
   final _phoneNumberKey = UniqueKey();
+  final _countryCodeKey = UniqueKey();
 
   @override
   void initState() {
     _inviteeNameController = TextEditingController(text: widget.inviteeName);
-    _inviteePhoneNumberController =
-        TextEditingController(text: widget.inviteePhoneNumber);
+    _inviteePhoneNumberController = TextEditingController(
+      text: widget.inviteePhoneNumber,
+    );
+    _inviteeCountryCodeController = TextEditingController(
+      text: widget.inviteeCountryCode,
+    );
     super.initState();
   }
 
@@ -57,22 +69,51 @@ class _InviteeFormState extends State<InviteeForm> {
           onChanged: (value) {
             widget.onInviteeInfoUpdate(
               value,
+              _inviteeCountryCodeController.text,
               _inviteePhoneNumberController.text,
             );
           },
         ),
-        TextField(
-          key: _phoneNumberKey,
-          controller: _inviteePhoneNumberController,
-          decoration: const InputDecoration(
-            labelText: 'Contact Number',
-          ),
-          onChanged: (value) {
-            widget.onInviteeInfoUpdate(
-              _inviteeNameController.text,
-              value,
-            );
-          },
+        Row(
+          children: [
+            Flexible(
+              flex: 3,
+              child: TextField(
+                key: _countryCodeKey,
+                controller: _inviteeCountryCodeController,
+                decoration: const InputDecoration(
+                  labelText: 'Country Code',
+                ),
+                onChanged: (value) {
+                  widget.onInviteeInfoUpdate(
+                    _inviteeNameController.text,
+                    value,
+                    _inviteePhoneNumberController.text,
+                  );
+                },
+              ),
+            ),
+            const SizedBox.square(
+              dimension: 8.0,
+            ),
+            Flexible(
+              flex: 7,
+              child: TextField(
+                key: _phoneNumberKey,
+                controller: _inviteePhoneNumberController,
+                decoration: const InputDecoration(
+                  labelText: 'Phone Number',
+                ),
+                onChanged: (value) {
+                  widget.onInviteeInfoUpdate(
+                    _inviteeNameController.text,
+                    _inviteeCountryCodeController.text,
+                    value,
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ],
     );
